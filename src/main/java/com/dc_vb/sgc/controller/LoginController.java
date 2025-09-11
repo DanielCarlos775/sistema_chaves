830474eed88e3d694f099c62e366a299cabd9245
Vitor Bonamigo Sassi <vitorbonamigosassi@gmail.com>
2025-09-10 21:21:09 -0300

    Adicionar botão de sair do app

--- src/main/java/com/dc_vb/sgc/controller/LoginController.java
+++ src/main/java/com/dc_vb/sgc/controller/LoginController.java
@@ -1,6 +1,11 @@
 package com.dc_vb.sgc.controller;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
 public class LoginController {
@@ -11,4 +16,22 @@ public class LoginController {
     @FXML
     private VBox root;
 
+    @FXML
+    private TextField usernameInput;
+
+    @FXML
+    PasswordField passwordInput;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    void handleLogin(ActionEvent event) {
+        System.out.printf("Fez Login\nUsuário: %s\nSenha: %s", usernameInput.getText(), passwordInput.getText());
+    }
+
+    @FXML
+    void closeApp(ActionEvent event) {
+        javafx.application.Platform.exit();
+    }
 }
