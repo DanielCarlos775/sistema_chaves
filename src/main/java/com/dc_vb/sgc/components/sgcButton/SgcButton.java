b5b7623efa8cad73038c6dc9aab76ff2f07d152b
Vitor Bonamigo Sassi <vitorbonamigosassi@gmail.com>
2025-09-30 21:28:07 -0300

    Criar classe reutilizável para o botão

---
+++ src/main/java/com/dc_vb/sgc/components/sgcButton/SgcButton.java
@@ -0,0 +1,22 @@
+package com.dc_vb.sgc.components.sgcButton;
+
+import com.dc_vb.sgc.util.SceneManager;
+import javafx.scene.control.Button;
+
+public class SgcButton extends Button {
+
+    public SgcButton() {
+        super("Button");
+        // Apply default styling, etc.
+        this.getStyleClass().add("sgc-button");
+
+            String styleSheetPath = "/css/components/SgcButton.css";
+        try {
+            String cssPath = getClass().getResource(styleSheetPath).toExternalForm();
+            this.getStylesheets().add(cssPath);
+        } catch (NullPointerException e) {
+            System.err.println("ERROR: Could not find stylesheet resource at path: " + styleSheetPath);
+            // Handle error or ensure pathing is correct
+        }
+    }
+}
