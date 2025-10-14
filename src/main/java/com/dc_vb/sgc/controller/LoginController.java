6a52e893c0639a255ab7a1693ef9630ea9448efc
Vitor Bonamigo Sassi <vitorbonamigosassi@gmail.com>
2025-10-14 19:32:18 -0300

    Estilizar inputs

--- src/main/java/com/dc_vb/sgc/controller/LoginController.java
+++ src/main/java/com/dc_vb/sgc/controller/LoginController.java
@@ -6,6 +6,7 @@ import com.dc_vb.sgc.service.AuthService;
 import com.dc_vb.sgc.util.SceneManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
@@ -14,44 +15,54 @@ import java.sql.SQLException;
 
 public class LoginController {
 
-    @FXML private TextField usernameInput;
+    @FXML
+    private Label emailLabel;
+    @FXML
+    private TextField emailInput;
+    @FXML
+    private Text errorTextEmail;
 
-    @FXML private PasswordField passwordInput;
+    @FXML
+    private Label passwordLabel;
+    @FXML
+    private PasswordField passwordInput;
+    @FXML
+    private Text errorTextPassword;
 
-    @FXML private Text errorText;
 
-    @FXML void initialize() {
-        errorText.setText("");
+    @FXML
+    void initialize() {
+        removeError();
     }
 
     @FXML
     void handleLogin(ActionEvent event) {
         System.out.printf(event + "\n");
-        String email = usernameInput.getText();
-        String senha = passwordInput.getText();
+        String email = emailInput.getText();
+        String password = passwordInput.getText();
+        removeError();
 
         try {
             if (email.isEmpty()) {
-                errorText.setText("O seu email está vazio");
-
-            } else if (senha.isEmpty()) {
-                errorText.setText("A sua senha está vazia");
+                setError("email");
+            } else if (password.isEmpty()) {
+                setError("password");
             } else {
                 UsuarioDAO usuarioDAO = new UsuarioDAO();
                 AuthService authService = new AuthService(usuarioDAO);
-                Usuario usuario =  authService.login(email, senha);
+                Usuario usuario = authService.login(email, password);
                 if (usuario != null) {
                     SceneManager.loadScene("MainLayout");
-                    errorText.setText("");
+                    removeError();
                 } else {
-                    errorText.setText("Os dados são inválidos");
+                    setError("email");
+                    setError("password");
+                    errorTextEmail.setText("Os dados são inválidos");
+                    errorTextPassword.setText("Os dados são inválidos");
                 }
-
             }
         } catch (SQLException e) {
-            System.out.printf(String.valueOf(e));
             throw new RuntimeException(e);
-        } finally {
         }
     }
 
@@ -65,5 +76,26 @@ public class LoginController {
     void handleGoToRegister(ActionEvent event) {
         System.out.printf(event + "\n");
         SceneManager.loadScene("Registrar");
+    }
+
+    void setError(String source) {
+        if (source.equals("email")) {
+            errorTextEmail.setText("O seu email está vazio");
+            emailLabel.getStyleClass().add("error-text");
+            emailInput.getStyleClass().add("error-border");
+        } else if (source.equals("password")){
+            errorTextPassword.setText("A sua senha está vazia");
+            passwordLabel.getStyleClass().add("error-text");
+            passwordInput.getStyleClass().add("error-border");
+        }
+    }
+
+    void removeError() {
+        errorTextEmail.setText("");
+        errorTextPassword.setText("");
+        emailInput.getStyleClass().remove("error-border");
+        emailLabel.getStyleClass().remove("error-text");
+        passwordInput.getStyleClass().remove("error-border");
+        passwordLabel.getStyleClass().remove("error-text");
     }
 }
